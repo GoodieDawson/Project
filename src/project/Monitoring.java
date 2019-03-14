4aa8e1a1c062ea0c62bb6d9b0578beb35191930b
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-03-14 17:16:13 +0000

    rsdga

--- src/project/Monitoring.java
+++ src/project/Monitoring.java
@@ -3,12 +3,34 @@
  */
 package project;
 
+import java.util.ArrayList;
+
 /**
  * @author Goodie Blake Dawson
  *
  */
 public class Monitoring extends Observatory{
 
+	private static ArrayList<Observatory> ObList = new ArrayList<Observatory>();
+	
+	public static double hiObAvg () {
+		double hiAvg = 0;
+		
+		for (Observatory ob : ObList) {
+			int obSum = 0;
+			
+			for (Galamsey galam : ob.getEvents()) {
+				obSum = obSum + galam.getColVal();
+			}
+			
+			double obAvg = obSum / ob.getEvents().size();
+			if (obAvg > hiAvg) { hiAvg = obAvg; }
+		}
+		
+		return hiAvg;
+	}
+	
+	
 	/**
 	 * @param args
 	 */
