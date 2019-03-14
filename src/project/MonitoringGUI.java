07fd0f908216fcef5298ef3bcbb1bc5e65ad7560
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-03-14 19:53:25 +0000

    d

---
+++ src/project/MonitoringGUI.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package project;
+
+/**
+ * @author Group_18
+ *
+ */
+public class MonitoringGUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
