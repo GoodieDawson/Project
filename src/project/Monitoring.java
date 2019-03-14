dc674f103f5eaca31b1c808debbf8c552f5d3e5e
lenryn <lenry.neequaye@ashesi.edu.gh>
2019-03-14 17:13:31 +0000

    largest val ever

--- src/project/Monitoring.java
+++ src/project/Monitoring.java
@@ -8,6 +8,25 @@ package project;
  *
  */
 public class Monitoring extends Observatory{
+	
+	/**
+	 * A method that returns the largest "galamsey" colour ever recorded.
+	 * @return
+	 */
+	
+	public static int maxValEver() {
+		int max = 0;
+		if (ObList.isEmpty()) {
+			System.out.println("The max value cannot be determined as there are no observatories");	
+		}
+		else {
+			for (Observatory obj: ObList) {
+					if (obj.maxColVal() > max) {max = obj.maxColVal();}
+			}
+		}
+		return max;
+	}
+		
 
 	/**
 	 * @param args
