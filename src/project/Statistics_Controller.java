6c95fe235819efd36c4acd3d7db08248834f6665
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-03-20 16:37:49 +0000

    we r getting there 2

--- src/project/Statistics_Controller.java
+++ src/project/Statistics_Controller.java
@@ -9,23 +9,142 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import javax.management.StringValueExp;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Statistics_Controller {
-    @FXML
-    private ChoiceBox chbx;
 
-    ObservableList<String> list = FXCollections.observableArrayList("stuff", "stuff2");
+    @FXML
+    private ChoiceBox<String> chbx1;
+
+    @FXML
+    private TableView<Observatory> obTable;
+
+    @FXML
+    private TableColumn<Observatory, String> obId;
+
+    @FXML
+    private TableColumn<Observatory, String> obName;
+
+    @FXML
+    private TableColumn<Observatory, String> obCount;
+
+    @FXML
+    private TableColumn<Observatory, String> obYear;
+
+    @FXML
+    private TableColumn<Observatory, String> obArea;
+
+    @FXML
+    private TableView<Galamsey> galtable;
+
+    @FXML
+    private TableColumn<Galamsey, String> galId;
+
+    @FXML
+    private TableColumn<Galamsey, String> vegCol;
+
+    @FXML
+    private TableColumn<Galamsey, String> colVal;
+
+    @FXML
+    private TableColumn<Galamsey, String> lon;
+
+    @FXML
+    private TableColumn<Galamsey, String> lat;
+
+    @FXML
+    private TableColumn<Galamsey, String> year;
+
+    @FXML
+    private ChoiceBox<String> chbx2;
+
+    @FXML
+    private Label result1;
+
+    ObservableList<Observatory> list1 = FXCollections.observableArrayList();
+    ObservableList<Galamsey> list2 = FXCollections.observableArrayList();
+    ObservableList<String> list3 = FXCollections.observableArrayList("Largest Value", "Average Value", "Galamsey List");
+    ObservableList<String> list4 = FXCollections.observableArrayList();
 
     @FXML
     private void initialize() {
-        chbx.setItems(list);
+        obId.setCellValueFactory(new PropertyValueFactory<>("obId"));
+        obName.setCellValueFactory(new PropertyValueFactory<>("obName"));
+        obCount.setCellValueFactory(new PropertyValueFactory<>("obCount"));
+        obYear.setCellValueFactory(new PropertyValueFactory<>("obYear"));
+        obArea.setCellValueFactory(new PropertyValueFactory<>("obArea"));
+
+        try {
+            //1. Creating Connection
+            Connection con = Database.startCon();
+
+            //2. Creating Statement
+            Statement stmnt = con.createStatement();
+
+            //3. Execute Query
+            ResultSet rs = stmnt.executeQuery("select * from observatory");
+
+            //4. Processing Result
+            while (rs.next()) {
+                list1.add(new Observatory (Integer.parseInt(rs.getString("obId")), rs.getString("obName"), rs.getString("obCount"), Integer.parseInt(rs.getString("obYear")), Double.parseDouble(rs.getString("obArea"))));
+            }
+
+            rs.close();
+            stmnt.close();
+            con.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        chbx1.setItems(list3);
+        obTable.setItems(list1);
+
+        galId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        vegCol.setCellValueFactory(new PropertyValueFactory<>("vegCol"));
+        colVal.setCellValueFactory(new PropertyValueFactory<>("colVal"));
+        lon.setCellValueFactory(new PropertyValueFactory<>("lon"));
+        lat.setCellValueFactory(new PropertyValueFactory<>("lat"));
+        year.setCellValueFactory(new PropertyValueFactory<>("year"));
+
+        try {
+            //1. Creating Connection
+            Connection con = Database.startCon();
+
+            //2. Creating Statement
+            Statement stmnt = con.createStatement();
+
+            //3. Execute Query
+            ResultSet rs = stmnt.executeQuery("select * from galamsey");
+
+            //4. Processing Result
+            while (rs.next()) {
+                list2.add(new Galamsey (Integer.parseInt(rs.getString("galamId")), rs.getString("vegCol"), Double.parseDouble(rs.getString("longitude")),Double.parseDouble(rs.getString("latitude")),Integer.parseInt(rs.getString("year"))));
+            }
+
+            rs.close();
+            stmnt.close();
+            con.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        galtable.setItems(list2);
     }
 
     @FXML
-    void loadMenu_Menu(ActionEvent event) throws IOException {
+    void loadMainMenu(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Main_Menu.fxml"));
         Scene scene = new Scene(root);
         Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -35,8 +154,114 @@ public class Statistics_Controller {
     }
 
     @FXML
-    void statnav(ActionEvent event) {
-        switch(chbx.getValue().toString()) {
+    void largestValue () {
+
+        int max = 0;
+
+        try {
+            //1. Creating Connection
+            Connection con = Database.startCon();
+
+            //2. Creating Statement
+            Statement stmnt = con.createStatement();
+
+            //3. Execute Query
+            Observatory obj = obTable.getSelectionModel().getSelectedItem();
+            ResultSet rs = stmnt.executeQuery("select * from galamsey where obId = " +obj.getObId());
+
+            while (rs.next()) {
+                if (max < Integer.parseInt(rs.getString("colVal"))) {max = Integer.parseInt(rs.getString("colVal"));}
+            }
+
+            stmnt.close();
+            con.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        result1.setText(String.valueOf(max));
+    }
+
+    @FXML
+    void averageValue() {
+        int sum = 0;
+        int count = 0;
+
+        try {
+            //1. Creating Connection
+            Connection con = Database.startCon();
+
+            //2. Creating Statement
+            Statement stmnt = con.createStatement();
+
+            //3. Execute Query
+            Observatory obj = obTable.getSelectionModel().getSelectedItem();
+            ResultSet rs = stmnt.executeQuery("select * from galamsey where obId = " +obj.getObId());
+
+            while (rs.next()) {
+                sum = sum  + Integer.parseInt(rs.getString("colVal"));
+                count++;
+            }
+
+
+            stmnt.close();
+            con.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        double avg = sum / count;
+        result1.setText(String.valueOf(avg));
+    }
+
+    @FXML
+    void galList() {
+        galtable.getItems().clear();
+        try {
+            //1. Creating Connection
+            Connection con = Database.startCon();
+
+            //2. Creating Statement
+            Statement stmnt = con.createStatement();
+
+            //3. Execute Query
+            Observatory obj = obTable.getSelectionModel().getSelectedItem();
+            ResultSet rs = stmnt.executeQuery("select * from galamsey where obId = " +obj.getObId());
+            while (rs.next()) {
+                list2.add(new Galamsey (Integer.parseInt(rs.getString("galamId")), rs.getString("vegCol"), Double.parseDouble(rs.getString("longitude")),Double.parseDouble(rs.getString("latitude")),Integer.parseInt(rs.getString("year"))));
+            }
+
+            stmnt.close();
+            con.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        galtable.setItems(list2);
+    }
+
+    @FXML
+    void obStatNav(ActionEvent event) {
+        switch(chbx1.getValue()) {
+            case "Largest Value" :
+                largestValue();
+                break;
+            case "Average Value" :
+                averageValue();
+                break;
+            case "Galamsey List" :
+                galList();
+                break;
+
+
+        }
+    }
+
+    @FXML
+    void galStatNav(ActionEvent event) {
+        switch(chbx2.getValue().toString()) {
 
         }
     }
