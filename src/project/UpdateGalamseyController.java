9bfad53f3d305e388a40cf93bf80ea8bd3807281
lenryn <lenry.neequaye@ashesi.edu.gh>
2019-03-22 02:19:14 +0000

    outta thisssssssssssss

--- src/project/UpdateGalamseyController.java
+++ src/project/UpdateGalamseyController.java
@@ -9,7 +9,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +20,9 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class UpdateGalamseyController {
+
+    @FXML
+    private Label lbl1;
 
     @FXML
     private ChoiceBox<String> vegcolchcbx;
@@ -41,6 +46,21 @@ public class UpdateGalamseyController {
     ObservableList<String> list2 = FXCollections.observableArrayList("Green", "Yellow", "Brown");
 
     private int id;
+
+    boolean validateInput(String longitude, String latitude, String year){
+        try {
+            double lon = Double.parseDouble(longitude);
+            double lat = Double.parseDouble(latitude);
+            int yr = Integer.parseInt(year);
+            return true;
+        }
+        catch (NumberFormatException e){
+            lbl1.setText("Update Failed: Please check your input");
+            lbl1.setTextFill(Paint.valueOf("#d64541"));
+            return false;
+        }
+    }
+
     @FXML
     void initialize(int galamId, String vegCol, double lat,double lon, int year) {
         id = galamId;
@@ -78,28 +98,34 @@ public class UpdateGalamseyController {
     }
     @FXML
     void UpdateGalamsey(ActionEvent event) {
-        try {
-            //1. Creating Connection
-            Connection con = Database.startCon();
+        if (validateInput(lontxtbx.getText(), lattxtbx.getText(), yrtxtbx.getText()) == true) {
+            try {
+                //1. Creating Connection
+                Connection con = Database.startCon();
 
-            //2. Creating Statement
-            Statement stmnt = con.createStatement();
+                //2. Creating Statement
+                Statement stmnt = con.createStatement();
 
-            //3. Execute Query
-            if (chcbx.getValue() == null) {chcbx.setValue("0");}
-            Galamsey obj = new Galamsey (vegcolchcbx.getValue(),
-                    Double.parseDouble(lontxtbx.getText()),
-                    Double.parseDouble(lattxtbx.getText()),
-                    Integer.parseInt(yrtxtbx.getText()),
-                    Integer.parseInt(chcbx.getValue()));
-            stmnt.executeUpdate("UPDATE galamsey SET vegCol = '" +obj.getVegCol()+ "', colVal = "+obj.getColVal()+", longitude = "+obj.getLon()+", latitude = "+obj.getLat()+", year = "+obj.getYear()+", obId = "+obj.getObId()+" WHERE galamId = " +id );
+                //3. Execute Query
+                if (chcbx.getValue() == null) {chcbx.setValue("0");}
+                Galamsey obj = new Galamsey (vegcolchcbx.getValue(),
+                        Double.parseDouble(lontxtbx.getText()),
+                        Double.parseDouble(lattxtbx.getText()),
+                        Integer.parseInt(yrtxtbx.getText()),
+                        Integer.parseInt(chcbx.getValue()));
+                stmnt.executeUpdate("UPDATE galamsey SET vegCol = '" +obj.getVegCol()+ "', colVal = "+obj.getColVal()+", longitude = "+obj.getLon()+", latitude = "+obj.getLat()+", year = "+obj.getYear()+", obId = "+obj.getObId()+" WHERE galamId = " +id );
 
-            stmnt.close();
-            con.close();
+                lbl1.setText("Update Successful");
+                lbl1.setTextFill(Paint.valueOf("#80B4B4"));
+
+                stmnt.close();
+                con.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+
         vegcolchcbx.setValue(null);
         lontxtbx.clear();
         lattxtbx.clear();
