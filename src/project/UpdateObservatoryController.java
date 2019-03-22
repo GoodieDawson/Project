20f76ea43318e15a34e32a7251845c2702e75f4b
lenryn <lenry.neequaye@ashesi.edu.gh>
2019-03-22 02:37:08 +0000

    outta thisssssssssssss

--- src/project/UpdateObservatoryController.java
+++ src/project/UpdateObservatoryController.java
@@ -6,7 +6,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -30,6 +32,22 @@ public class UpdateObservatoryController {
     private int id;
 
     @FXML
+    private Label lbl1;
+
+    boolean validateInput(String year, String area){
+        try {
+            int yr = Integer.parseInt(year);
+            double lat = Double.parseDouble(area);
+            return true;
+        }
+        catch (NumberFormatException e){
+            lbl1.setText("Update Failed: Please check your input");
+            lbl1.setTextFill(Paint.valueOf("#d64541"));
+            return false;
+        }
+    }
+
+    @FXML
     void initialize(int obId, String obName, String count, int year, double area) {
         id = obId;
         nmetxtbx.setText(obName);
@@ -50,29 +68,35 @@ public class UpdateObservatoryController {
 
     @FXML
     void updateObservatory(ActionEvent event) {
-        try {
-            //1. Creating Connection
-            Connection con = Database.startCon();
+        if (validateInput(yrtxtbx.getText(), areatxtbx.getText()) == true) {
+            try {
+                //1. Creating Connection
+                Connection con = Database.startCon();
 
-            //2. Creating Statement
-            Statement stmnt = con.createStatement();
+                //2. Creating Statement
+                Statement stmnt = con.createStatement();
 
-            //3. Execute Query
-            Observatory obj = new Observatory (nmetxtbx.getText(),
-                    countxtbx.getText(),
-                    Integer.parseInt(yrtxtbx.getText()),
-                    Double.parseDouble(areatxtbx.getText()));
-            stmnt.executeUpdate("UPDATE observatory SET obName = '" +obj.getObName()+"', obcount = '"+obj.getObCount()+"', obYear = "+obj.getObYear()+", obarea = "+obj.getObArea()+" WHERE obId = " +id );
+                //3. Execute Query
+                Observatory obj = new Observatory (nmetxtbx.getText(),
+                        countxtbx.getText(),
+                        Integer.parseInt(yrtxtbx.getText()),
+                        Double.parseDouble(areatxtbx.getText()));
+                stmnt.executeUpdate("UPDATE observatory SET obName = '" +obj.getObName()+"', obcount = '"+obj.getObCount()+"', obYear = "+obj.getObYear()+", obarea = "+obj.getObArea()+" WHERE obId = " +id );
 
-            stmnt.close();
-            con.close();
+                lbl1.setText("Update Successful");
+                lbl1.setTextFill(Paint.valueOf("#80B4B4"));
+
+                stmnt.close();
+                con.close();
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+            nmetxtbx.clear();
+            countxtbx.clear();
+            yrtxtbx.clear();
+            areatxtbx.clear();
         }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        nmetxtbx.clear();
-        countxtbx.clear();
-        yrtxtbx.clear();
-        areatxtbx.clear();
     }
+
 }
